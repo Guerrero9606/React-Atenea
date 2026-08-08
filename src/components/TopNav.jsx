6e17226346eea90306e01ed7f83ffe14acc0f89f
Edgar Camilo Guerrero <edgarguerrero37@gmail.com>
2026-08-08 09:21:11 -0500

    Uso de useEffect, componentes Contador, Reloj, buscador

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,4 +1,6 @@
 import './TopNav.css';
+import Buscardor from './Buscardor';
+import RelojDigital from './RelojDigital';
 
 function TopNav() {
     const usuarioActual = "Juan Perez";
@@ -6,13 +8,15 @@ function TopNav() {
     return (
         <header className='topnav'>
             <div className='buscador'>
-                <input type="text" placeholder='Buscar...' className='input-buscar'/>
+                {/* <input type="text" placeholder='Buscar...' className='input-buscar'/>  */}
+                <Buscardor clase="input-buscar"/>
             </div>
             <div className='perfil-usuario'>
                 <span className='notificaciones'>🔔</span>
                 <div className='avatar'>JP</div>
                 <span className='nombre-usuario'>Hola, {usuarioActual}</span>
             </div>
+            {/* <RelojDigital /> */}
         </header>
     );
 }
--- src/pages/PanelGeneral.jsx
+++ src/pages/PanelGeneral.jsx
@@ -1,4 +1,6 @@
-import ContadorVentas from "../components/ContadorVentas";
+import ContadorGeneral from "../components/ContadorGeneral";
+// import ContadorVentas from "../components/ContadorVentas";
+import RelojDigital from "../components/RelojDigital";
 
 export default function PanelGeneral(){
     return(
@@ -6,7 +8,8 @@ export default function PanelGeneral(){
             <h1>Panel General</h1>
             <p>Bienvenido al sistema. Aqui estan las estadisticas</p>
 
-            <ContadorVentas />
+            <ContadorGeneral />
+            <RelojDigital />
 
         </div>
     );
